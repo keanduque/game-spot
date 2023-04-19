689326fc032ea7187061f4dcc0b500f2fd591fd2
Kean Duque <vizen27@users.noreply.github.com>
2023-04-19 14:55:15 +0200

    optimize image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppeImageUrl from "../services/image-url";
 
 interface Props {
 	game: Game;
